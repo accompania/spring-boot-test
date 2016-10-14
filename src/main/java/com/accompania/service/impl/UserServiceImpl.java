29a3c18d39d4c8278713989ea62186666d2927b9
Jean-François Beauchef <jean-francois@beauchef.com>
2016-10-13 21:36:45 -0400

    Added tests + 1 user

--- src/main/java/com/accompania/service/impl/UserServiceImpl.java
+++ src/main/java/com/accompania/service/impl/UserServiceImpl.java
@@ -14,12 +14,14 @@ public class UserServiceImpl implements UserService {
 
   private static final User JOHN = new User(1L, "John", "Doe");
   private static final User JANE = new User(2L, "Jane", "White");
+  private static final User BOB = new User(3L, "Bob", "Dole");
 
   private static final Map<Long, User> USERS;
   static {
     Map<Long, User> users = new HashMap<>();
     users.put(JOHN.getId(), JOHN);
     users.put(JANE.getId(), JANE);
+    users.put(BOB.getId(), BOB);
     USERS = Collections.unmodifiableMap(users);
   }
 
